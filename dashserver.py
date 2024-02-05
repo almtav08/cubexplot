a4e401d404016c2567feed63bc2f9b275b1ec377
Alex Martínez Martínez <alexmartinez3107@gmail.com>
2024-02-05 10:00:52 +0100

    add plot

---
+++ dashserver.py
@@ -0,0 +1,30 @@
+import dash
+from dash import dcc, html
+import plotly.graph_objects as go
+import polars as pl
+
+# Crear la aplicación Dash
+app = dash.Dash(__name__)
+
+# Crear datos de ejemplo
+data = pl.read_csv("plots.csv")
+
+# Crear figura con Plotly Express
+limit = data.shape[0]
+symbols = ['circle' for _ in range(limit)]
+colors = ['blue' for _ in range(limit)]
+marker=dict(size=2, color=colors, opacity=0.8, symbol=symbols)
+
+fig = go.Figure(data=[go.Scatter3d(x=data['percentage'], y=data['concepts'], z=data['secs'], mode='markers', marker=marker)])
+fig.update_layout(scene=dict(xaxis_title='Percentage ', yaxis_title='N concepts', zaxis_title='Seconds'), margin=dict(l=0, r=0, t=0, b=0))
+
+# Definir el diseño de la aplicación Dash
+app.layout = html.Div([
+    dcc.Graph(id='scatter-plot', figure=fig, style={'width': '100%', 'height': '100%'})
+], style={'width': '98dvw', 'height': '98dvh'})
+
+app.title = 'Video metadata plotting'
+
+# Iniciar la aplicación Dash
+if __name__ == '__main__':
+    app.run_server(debug=True)
